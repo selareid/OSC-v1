5a812f1d712592847c9feccff660c1f4813f7124
starwar15432 <danielselareid40@gmail.com>
2016-09-20 19:28:20 +1200

    Update defenceHandler.js

--- defenceHandler.js
+++ defenceHandler.js
@@ -23,7 +23,7 @@ module.exports = {
 
     getHostileCreeps: function (room, allyUsername) {
         var hostileCreepsInRoom = room.find(FIND_HOSTILE_CREEPS, {filter: (c) => allyUsername.includes(c.owner.username) == false});
-        return hostileCreepsInRoom.owner.username;
+        return hostileCreepsInRoom;
     },
 
     isUnderAttack: function (room, allyUsername) {
