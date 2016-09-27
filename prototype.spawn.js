76109a13f6216879c9ea80fb40c2611e5f03b35c
Daniel Reid <danielselareid40@gmail.com>
2016-09-27 14:07:22 +1300

    changed the if else statements to a switch statement

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -5,127 +5,117 @@ module.exports = function () {
             var numberOfParts;
             var body = [];
 
-            if (roleName == 'harvester') {
-                numberOfParts = Math.floor(((energy - (energy * amountToSave)) - 100) / 100);
+            switch(roleName) {
+                case 'harvester':
+                    numberOfParts = Math.floor(((energy - (energy * amountToSave)) - 100) / 100);
 
-                if (numberOfParts > 6) {
-                    numberOfParts = 6;
-                }
-                body.push(MOVE);
-                body.push(CARRY);
-                for (let i = 0; i < numberOfParts; i++) {
+                    if (numberOfParts > 6) {
+                        numberOfParts = 6;
+                    }
+                    body.push(MOVE);
+                    body.push(CARRY);
+                    for (let i = 0; i < numberOfParts; i++) {
+                        body.push(WORK);
+                    }
+                    return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
+                case 'distributor':
+                    numberOfParts = Math.floor(((energy - (energy * amountToSave)) - 100) / 150);
                     body.push(WORK);
-                }
-                return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
-            }
-            else if (roleName == 'distributor') {
-                numberOfParts = Math.floor(((energy - (energy * amountToSave)) - 100) / 150);
-                body.push(WORK);
-                for (let i = 0; i < numberOfParts; i++) {
-                    body.push(CARRY);
-                    body.push(CARRY);
-                    body.push(MOVE);
-                }
-                return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
-            }
-            else if (roleName == 'carrier') {
-                numberOfParts = Math.floor(((energy - (energy * amountToSave)) - 100) / 150);
-                body.push(WORK);
-                for (let i = 0; i < numberOfParts; i++) {
-                    body.push(CARRY);
-                    body.push(CARRY);
-                    body.push(MOVE);
-                }
-                return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
-            }
-            else if (roleName == 'warrior') {
+                    for (let i = 0; i < numberOfParts; i++) {
+                        body.push(CARRY);
+                        body.push(CARRY);
+                        body.push(MOVE);
+                    }
+                    return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
+                case 'carrier':
+                    numberOfParts = Math.floor(((energy - (energy * amountToSave)) - 100) / 150);
+                    body.push(WORK);
+                    for (let i = 0; i < numberOfParts; i++) {
+                        body.push(CARRY);
+                        body.push(CARRY);
+                        body.push(MOVE);
+                    }
+                    return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
+                case 'warrior':
+                    var numberOfRanged = _.sum(Game.creeps, (c) => c.memory.role == 'warrior' && c.memory.room == room.name && c.getActiveBodyparts(RANGED_ATTACK) >= 1);
 
-                var numberOfRanged = _.sum(Game.creeps, (c) => c.memory.role == 'warrior' && c.memory.room == room.name && c.getActiveBodyparts(RANGED_ATTACK) >= 1);
-
-                if (numberOfRanged < 2) {
+                    if (numberOfRanged < 2) {
+                        numberOfParts = Math.floor((energy - (energy * amountToSave)) / 200);
+                        if (numberOfParts > 4) {
+                            numberOfParts = 4;
+                        }
+                        for (let i = 0; i < numberOfParts; i++) {
+                            body.push(MOVE);
+                            body.push(RANGED_ATTACK);
+                        }
+                    }
+                    else {
+                        numberOfParts = Math.floor((energy - (energy * amountToSave)) / 210);
+                        if (numberOfParts > 3) {
+                            numberOfParts = 3;
+                        }
+                        for (let i = 0; i < numberOfParts; i++) {
+                            body.push(MOVE);
+                            body.push(ATTACK);
+                            body.push(ATTACK);
+                        }
+                    }
+                    return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
+                case 'upgrader':
+                    numberOfParts = Math.floor(((energy - (energy * amountToSave)) - 50) / 150);
+                    body.push(MOVE);
+                    for (let i = 0; i < numberOfParts; i++) {
+                        body.push(WORK);
+                        body.push(CARRY);
+                    }
+                    return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
+                case 'builder':
                     numberOfParts = Math.floor((energy - (energy * amountToSave)) / 200);
-                    if (numberOfParts > 4) {
-                        numberOfParts = 4;
-                    }
+
                     for (let i = 0; i < numberOfParts; i++) {
+                        body.push(WORK);
+                        body.push(CARRY);
                         body.push(MOVE);
-                        body.push(RANGED_ATTACK);
                     }
-                }
-                else {
-                    numberOfParts = Math.floor((energy - (energy * amountToSave)) / 210);
-                    if (numberOfParts > 3) {
-                        numberOfParts = 3;
-                    }
+                    return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
+                case 'repairer':
+                    numberOfParts = Math.floor((energy - (energy * amountToSave)) / 200);
+
                     for (let i = 0; i < numberOfParts; i++) {
+                        body.push(WORK);
+                        body.push(CARRY);
                         body.push(MOVE);
-                        body.push(ATTACK);
-                        body.push(ATTACK);
                     }
-                }
+                    return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
+                case 'defenceManager':
+                    numberOfParts = Math.floor((energy - (energy * amountToSave)) / 200);
 
-                return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
-            }
-            else if (roleName == 'upgrader') {
-                numberOfParts = Math.floor(((energy - (energy * amountToSave)) - 50) / 150);
-                body.push(MOVE);
-                for (let i = 0; i < numberOfParts; i++) {
-                    body.push(WORK);
-                    body.push(CARRY);
-                }
-                return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
-            }
-            else if (roleName == 'builder') {
-                numberOfParts = Math.floor((energy - (energy * amountToSave)) / 200);
+                    for (let i = 0; i < numberOfParts; i++) {
+                        body.push(WORK);
+                        body.push(CARRY);
+                        body.push(MOVE);
+                    }
+                    return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
+                case 'landlord':
+                    numberOfParts = Math.floor((energy - (energy * amountToSave)) / 650);
 
-                for (let i = 0; i < numberOfParts; i++) {
-                    body.push(WORK);
-                    body.push(CARRY);
-                    body.push(MOVE);
-                }
-                return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
-            }
-            else if (roleName == 'repairer') {
-                numberOfParts = Math.floor((energy - (energy * amountToSave)) / 200);
+                    for (let i = 0; i < numberOfParts; i++) {
+                        body.push(CLAIM);
+                        body.push(MOVE);
+                    }
+                    return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
+                case 'otherRoomCreep':
+                    numberOfParts = Math.floor((energy - (energy * amountToSave)) / 200);
 
-                for (let i = 0; i < numberOfParts; i++) {
-                    body.push(WORK);
-                    body.push(CARRY);
-                    body.push(MOVE);
-                }
-                return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
+                    for (let i = 0; i < numberOfParts; i++) {
+                        body.push(WORK);
+                        body.push(MOVE);
+                        body.push(CARRY);
+                    }
+                    return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
+                default:
+                    return 'SPAWNING ERROR';
             }
-            else if (roleName == 'defenceManager') {
-                numberOfParts = Math.floor((energy - (energy * amountToSave)) / 200);
 
-                for (let i = 0; i < numberOfParts; i++) {
-                    body.push(WORK);
-                    body.push(CARRY);
-                    body.push(MOVE);
-                }
-                return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
-            }
-            else if (roleName == 'landlord') {
-                numberOfParts = Math.floor((energy - (energy * amountToSave)) / 650);
-
-                for (let i = 0; i < numberOfParts; i++) {
-                    body.push(CLAIM);
-                    body.push(MOVE);
-                }
-                return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
-            }
-            else if (roleName == 'otherRoomCreep') {
-                numberOfParts = Math.floor((energy - (energy * amountToSave)) / 200);
-
-                for (let i = 0; i < numberOfParts; i++) {
-                    body.push(WORK);
-                    body.push(MOVE);
-                    body.push(CARRY);
-                }
-                return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
-            }
-            else {
-                return 'SPAWNING ERROR';
-            }
         }
 };
