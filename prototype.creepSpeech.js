afdf26fa5f0f667928ba4686c3e430e6a95fff66
Daniel Reid <danielselareid40@gmail.com>
2016-09-30 14:19:21 +1300

    started creepsSpeech

--- prototype.creepSpeech.js
+++ prototype.creepSpeech.js
@@ -1,10 +1,44 @@
 require('global');
 
 module.exports = function () {
-    Creep.prototype.speech = function (creep, doingWhat) {
+    Creep.prototype.creepSpeech = function (room, creep, doingWhat) {
 
+        switch (doingWhat) {
+            case 'movingToSource':
 
+                break;
+            case 'harvesting':
 
+                break;
+        }
 
+    },
+         function movingToSource(creep) {
+        switch (Game.time % 8){
+            case 0:
+                creep.say("I'm on");
+                break;
+            case 1:
+                creep.say('my way');
+                break;
+            case 2:
+                creep.say('from spawn');
+                break;
+            case 3:
+                creep.say('to source today');
+                break;
+            case 4:
+                creep.say('uh-huh');
+                break;
+            case 5:
+                creep.say('uh-huh');
+                break;
+            case 6:
+                creep.say('uh-huh');
+                break;
+            case 7:
+                creep.say('uh-huh');
+                break;
+        }
     }
 };
