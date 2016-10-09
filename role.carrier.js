1ff5d6e0e21bbc1b242f4ac490bc6338f59b68b2
Daniel Reid <danielselareid40@gmail.com>
2016-10-09 19:38:07 +1300

    carriers made better! ;)

--- role.carrier.js
+++ role.carrier.js
@@ -43,12 +43,17 @@ module.exports = {
 
                 container = Game.getObjectById(creep.memory.container);
 
+                if (container.store[RESOURCE_ENERGY] >= 300) {
+
                     if (container) {
                         if (creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                             creep.moveTo(container)
                         }
                     }
-
+                }
+                else {
+                    delete creep.memory.container;
+                }
             }
 
         }
