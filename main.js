834846342487fffa135bb7c933f74e96ba48a279
Daniel Reid <danielselareid40@gmail.com>
2016-10-18 14:14:38 +1300

    added try catch statement

--- main.js
+++ main.js
@@ -44,18 +44,26 @@ module.exports.loop = function () {
             var spawn = room.find(FIND_MY_SPAWNS)[0];
             if (spawn) {
 
-                if (Memory.rooms) {
-                    if (!Memory.rooms[room]) {
-                        Memory.rooms[room] = {};
+                try {
+                    if (Memory.rooms) {
+                        if (!Memory.rooms[room]) {
+                            Memory.rooms[room] = {};
+                        }
+                    }
+                    else {
+                        Memory.rooms = {};
                     }
                 }
-                else {
-                    Memory.rooms = {};
+                catch (err) {
+                    if (err !== null && err !== undefined) {
+                        Game.notify("Error in Memory.room logic: \n" + err + "\n " + err.stack);
+                        console.log("Error in Memory.room logic: \n" + err + "\n" + err.stack);
+                    }
                 }
 
                 roomHandler.run(room);
             }
-    }
+        }
 
     try {
 
