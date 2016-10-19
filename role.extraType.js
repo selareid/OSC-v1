0bb1eb6a33a8bdc50b6f1619f1e502f6678d09ae
Daniel Reid <danielselareid40@gmail.com>
2016-10-19 16:03:36 +1300

    quick

--- role.extraType.js
+++ role.extraType.js
@@ -1,7 +1,7 @@
 require('global');
 
 module.exports = {
-    run: function(creep) {
+    run: function(room, creep, roomToGoTo) {
         if (creep.memory.working == true && creep.carry.energy == 0) {
             creep.memory.working = false;
         }
@@ -10,28 +10,40 @@ module.exports = {
         }
 
         if (creep.memory.working == true) {
-            var spawn = creep.room.find(FIND_MY_SPAWNS, {filter: (s) => s.energy < s.energyCapacity});
-
-            if (spawn) {
-                if (creep.transfer(spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(spawn);
-                }
+            if (creep.room.name != room.name) {
+                let roomPos2 = new RoomPosition(12, 25, room.name);
+                creep.moveTo(roomPos2);
             }
             else {
-                if (creep.pos.x == 12 && creep.pos.y == 25) {
-                    creep.drop(RESOURCE_ENERGY);
+                var spawn = creep.room.find(FIND_MY_SPAWNS, {filter: (s) => s.energy < s.energyCapacity});
+
+                if (spawn) {
+                    if (creep.transfer(spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(spawn);
+                    }
                 }
                 else {
-                    creep.moveTo(12, 25);
+                    if (creep.pos.x == 12 && creep.pos.y == 25) {
+                        creep.drop(RESOURCE_ENERGY);
+                    }
+                    else {
+                        creep.moveTo(12, 25);
+                    }
                 }
             }
         }
         else {
-            var storage = creep.room.storage;
+            if (creep.room.name != roomToGoTo) {
+                let roomPos1 = new RoomPosition(26, 20, roomToGoTo);
+                creep.moveTo(roomPos1);
+            }
+            else {
+                var storage = creep.room.storage;
 
-            if (storage && storage.store[RESOURCE_ENERGY] > 0) {
-                if (creep.withdraw(storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(storage);
+                if (storage && storage.store[RESOURCE_ENERGY] > 0) {
+                    if (creep.withdraw(storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(storage);
+                    }
                 }
             }
         }
--- role.otherRoomCreep.js
+++ role.otherRoomCreep.js
@@ -11,58 +11,58 @@ module.exports = {
 
         if (roomToGoTo) {
 
-            if (creep.memory.working == false && creep.room.name != roomToGoTo) {
-                let roomPos = new RoomPosition(26, 20, roomToGoTo);
-                creep.moveTo(roomPos);
-            }
-            else if (creep.memory.working == false) {
-                if (!creep.memory.type) {
-                    if (_.sum(Game.creeps, (c) => c.memory.type == 'type1') < 1) {
-                        creep.memory.type = 'type1';
-                    }
-                    else if (_.sum(Game.creeps, (c) => c.memory.type == 'type2') < 4) {
-                        creep.memory.type = 'type2';
-                    }
-                    if (_.sum(Game.creeps, (c) => c.memory.type == 'type3') < 1) {
-                        creep.memory.type = 'type3';
-                    }
-                    if (_.sum(Game.creeps, (c) => c.memory.type == 'type4') < 0) {
-                        creep.memory.type = 'type4';
-                    }
-                    else {
-                        creep.memory.type = 'type2';
-                    }
-                }
-                else {
-
-                    // switch (creep.memory.type) {
-                    //     case 'type1':
-                    //         type1.run(creep);
-                    //         break;
-                    //     case 'type2':
-                    //         type2.run(creep);
-                    //         break;
-                    //     case 'type3':
-                    //         type3.run(creep);
-                    //         break;
-                    //     case 'type4':
-                    //         type4.run(creep);
-                    //         break;
-                    //     default:
-                    //         creep.say('ERR TYPE');
-                    // }
-
-extraType.run(creep);
-
-                }
-            }
-            else if (creep.memory.working == true && creep.room.name != room.name) {
-                let roomPos = new RoomPosition(12, 25, room.name);
-                creep.moveTo(roomPos);
-            }
-            else {
-                extraType.run(creep);
-            }
+//             if (creep.memory.working == false && creep.room.name != roomToGoTo) {
+//                 let roomPos = new RoomPosition(26, 20, roomToGoTo);
+//                 creep.moveTo(roomPos);
+//             }
+//             else if (creep.memory.working == false) {
+//                 if (!creep.memory.type) {
+//                     if (_.sum(Game.creeps, (c) => c.memory.type == 'type1') < 1) {
+//                         creep.memory.type = 'type1';
+//                     }
+//                     else if (_.sum(Game.creeps, (c) => c.memory.type == 'type2') < 4) {
+//                         creep.memory.type = 'type2';
+//                     }
+//                     if (_.sum(Game.creeps, (c) => c.memory.type == 'type3') < 1) {
+//                         creep.memory.type = 'type3';
+//                     }
+//                     if (_.sum(Game.creeps, (c) => c.memory.type == 'type4') < 0) {
+//                         creep.memory.type = 'type4';
+//                     }
+//                     else {
+//                         creep.memory.type = 'type2';
+//                     }
+//                 }
+//                 else {
+//
+//                     // switch (creep.memory.type) {
+//                     //     case 'type1':
+//                     //         type1.run(creep);
+//                     //         break;
+//                     //     case 'type2':
+//                     //         type2.run(creep);
+//                     //         break;
+//                     //     case 'type3':
+//                     //         type3.run(creep);
+//                     //         break;
+//                     //     case 'type4':
+//                     //         type4.run(creep);
+//                     //         break;
+//                     //     default:
+//                     //         creep.say('ERR TYPE');
+//                     // }
+//
+// extraType.run(creep);
+//
+//                 }
+//             }
+//             else if (creep.memory.working == true && creep.room.name != room.name) {
+//                 let roomPos = new RoomPosition(12, 25, room.name);
+//                 creep.moveTo(roomPos);
+//             }
+//             else {
+                extraType.run(room, creep, otherRoomCreeps);
+            //}
 
         }
     }
