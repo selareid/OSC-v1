3f0ab1f5d49268dcedb882581f8a2339c82e7a9b
Daniel Reid <danielselareid40@gmail.com>
2016-10-19 15:40:12 +1300

    quick

---
+++ role.extraType.js
@@ -0,0 +1,30 @@
+require('global');
+
+module.exports = {
+    run: function(creep) {
+        if (creep.memory.working == true && creep.carry.energy == 0) {
+            creep.memory.working = false;
+        }
+        else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.working = true;
+        }
+
+        if (creep.memory.working == true) {
+            if (creep.pos.x == 12 && creep.pos.y == 25) {
+                creep.drop(RESOURCE_ENERGY);
+            }
+            else {
+                creep.moveTo(12, 25);
+            }
+        }
+        else {
+            var storage = creep.room.storage;
+
+            if (storage && storage.store[RESOURCE_ENERGY] > 0) {
+                if (creep.withdraw(storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(storage);
+                }
+            }
+        }
+    }
+};
--- role.otherRoomCreep.js
+++ role.otherRoomCreep.js
@@ -1,20 +1,21 @@
 require('global');
 
-var type1 = require ('role.type1'); //RCL upgrader
-var type2 = require ('role.type2'); //builder
-var type3 = require ('role.type3'); //repairer
-var type4 = require ('role.type4'); //wallRepairer and tower replenisher
+const type1 = require ('role.type1'); //RCL upgrader
+const type2 = require ('role.type2'); //builder
+const type3 = require ('role.type3'); //repairer
+const type4 = require ('role.type4'); //wallRepairer and tower replenisher
+const extraType = require ('role.exxtraType');
 
 module.exports = {
     run: function (room, creep, roomToGoTo) {
 
         if (roomToGoTo) {
 
-            if (creep.room.name != roomToGoTo) {
-                var roomPos = new RoomPosition(26, 20, roomToGoTo);
+            if (creep.memory.working == false && creep.room.name != roomToGoTo) {
+                let roomPos = new RoomPosition(26, 20, roomToGoTo);
                 creep.moveTo(roomPos);
             }
-            else {
+            else if (creep.memory.working == false) {
                 if (!creep.memory.type) {
                     if (_.sum(Game.creeps, (c) => c.memory.type == 'type1') < 1) {
                         creep.memory.type = 'type1';
@@ -34,24 +35,30 @@ module.exports = {
                 }
                 else {
 
-                    switch (creep.memory.type) {
-                        case 'type1':
-                            type1.run(creep);
-                            break;
-                        case 'type2':
-                            type2.run(creep);
-                            break;
-                        case 'type3':
-                            type3.run(creep);
-                            break;
-                        case 'type4':
-                            type4.run(creep);
-                            break;
-                        default:
-                            creep.say('ERR TYPE');
-                    }
+                    // switch (creep.memory.type) {
+                    //     case 'type1':
+                    //         type1.run(creep);
+                    //         break;
+                    //     case 'type2':
+                    //         type2.run(creep);
+                    //         break;
+                    //     case 'type3':
+                    //         type3.run(creep);
+                    //         break;
+                    //     case 'type4':
+                    //         type4.run(creep);
+                    //         break;
+                    //     default:
+                    //         creep.say('ERR TYPE');
+                    // }
+
+extraType.run(creep);
 
                 }
+            }
+            else if (creep.memory.working == true && creep.room.name != room.name) {
+                let roomPos = new RoomPosition(12, 25, roomToGoTo);
+                creep.moveTo(roomPos);
             }
 
         }
