26c426cd243debd3dd0ebd62a0d28573efadd0f2
Daniel Reid <danielselareid40@gmail.com>
2016-10-22 15:41:34 +1300

    added auto road creation to remoteHarvester

--- role.remoteHarvester.js
+++ role.remoteHarvester.js
@@ -2,64 +2,71 @@ require('global');
 
 module.exports = {
     run: function (room, creep) {
-            //changes state
-            if (creep.memory.working == true && creep.carry.energy == 0) {
-                creep.memory.working = false;
+
+        if (!creep.pos.look(LOOK_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_ROAD})[0]) {
+            creep.room.createConstructionSite(creep.pos, STRUCTURE_ROAD);
+        }
+
+        //changes state
+        if (creep.memory.working == true && creep.carry.energy == 0) {
+            creep.memory.working = false;
+        }
+        else if (creep.memory.working == false && creep.carry.energy >= creep.carryCapacity) {
+            creep.memory.working = true;
+        }
+
+        // if working if true do stuff or else mine
+        if (creep.memory.working == true) {
+
+            //if container found put transfer energy to container if container full drop energy
+
+            var container = creep.pos.findInRange(FIND_STRUCTURES, 1, {
+                filter: (s) => s.structureType == STRUCTURE_CONTAINER
+                && _.sum(s.store) < s.storeCapacity
+            })[0];
+
+            if (container) {
+                creep.creepSpeech(room, 'droppingEnergyContainer');
+                creep.transfer(container, RESOURCE_ENERGY);
             }
-            else if (creep.memory.working == false && creep.carry.energy >= creep.carryCapacity) {
-                creep.memory.working = true;
+            else {
+                creep.creepSpeech(room, 'droppingEnergy');
+                creep.drop(RESOURCE_ENERGY);
             }
+        }
+        else {
 
-            // if working if true do stuff or else mine
-            if (creep.memory.working == true) {
+            if (!creep.memory.source) {
+                var harvesters = _.filter(Game.creeps, c => c.memory.role == 'remoteHarvester' && c.memory.room == room.name && c.spawning == false && c.name != creep.name);
+                var takenSources = [];
 
-                //if container found put transfer energy to container if container full drop energy
-
-                var container = creep.pos.findInRange(FIND_STRUCTURES, 1, {filter: (s) => s.structureType == STRUCTURE_CONTAINER
-                && _.sum(s.store) < s.storeCapacity})[0];
-
-                if (container) {
-                    creep.creepSpeech(room, 'droppingEnergyContainer');
-                    creep.transfer(container, RESOURCE_ENERGY);
+                for (let harvester of harvesters) {
+                    if (harvester.memory.source) {
+                        takenSources.push(harvester.memory.source);
+                    }
                 }
-                else {
-                    creep.creepSpeech(room, 'droppingEnergy');
-                    creep.drop(RESOURCE_ENERGY);
+
+                creep.memory.source = this.findSource(room, creep, takenSources).id;
+                console.log('harvesters calculating source');
+            }
+
+            var source = Game.getObjectById(creep.memory.source);
+
+            if (source) {
+                switch (creep.harvest(source)) {
+                    case ERR_NOT_IN_RANGE:
+                        creep.creepSpeech(room, 'movingToSource');
+                        creep.moveTo(source, {reusePath: 10});
+                        break;
+                    case OK:
+                        creep.creepSpeech(room, 'harvesting');
+                        break;
                 }
             }
             else {
-
-                if (!creep.memory.source) {
-                    var harvesters = _.filter(Game.creeps, c => c.memory.role == 'remoteHarvester' && c.memory.room == room.name && c.spawning == false && c.name != creep.name);
-                    var takenSources = [];
-
-                    for (let harvester of harvesters) {
-                        if (harvester.memory.source) {
-                            takenSources.push(harvester.memory.source);
-                        }
-                    }
-
-                    creep.memory.source = this.findSource(room, creep, takenSources).id;
-                    console.log('harvesters calculating source');
-                }
-
-                var source = Game.getObjectById(creep.memory.source);
-
-                if (source) {
-                        switch (creep.harvest(source)) {
-                            case ERR_NOT_IN_RANGE:
-                                creep.creepSpeech(room, 'movingToSource');
-                            creep.moveTo(source, {reusePath: 10});
-                                break;
-                            case OK:
-                                creep.creepSpeech(room, 'harvesting');
-                                break;
-                        }
-                }
-                else {
-                    delete creep.memory.source;
-                }
+                delete creep.memory.source;
             }
+        }
     },
 
     findSource: function (room, creep, takenSources) {
