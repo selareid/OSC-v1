1d77c543467d69d5305a56f958b4667f73746abe
Daniel Reid <danielselareid40@gmail.com>
2016-10-25 13:24:13 +1300

    added quick check

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -11,34 +11,39 @@ module.exports = function () {
                 case 'harvester':
                     numberOfParts = Math.floor(((energy - (energy * amountToSave)) - 100) / 100);
 
-                    if (numberOfParts > 6) {
-                        numberOfParts = 6;
-                    }
-                    body.push(MOVE);
-                    body.push(CARRY);
-                    for (let i = 0; i < numberOfParts; i++) {
-                        body.push(WORK);
+                    if (numberOfParts > 0) {
+                        if (numberOfParts > 6) {
+                            numberOfParts = 6;
+                        }
+                        body.push(MOVE);
+                        body.push(CARRY);
+                        for (let i = 0; i < numberOfParts; i++) {
+                            body.push(WORK);
+                        }
                     }
                     return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
                 case 'distributor':
                     numberOfParts = Math.floor((energy - (energy * amountToSave)) / 150);
-                    for (let i = 0; i < numberOfParts; i++) {
-                        body.push(CARRY);
-                        body.push(CARRY);
-                        body.push(MOVE);
+                    if (numberOfParts > 0) {
+                        for (let i = 0; i < numberOfParts; i++) {
+                            body.push(CARRY);
+                            body.push(CARRY);
+                            body.push(MOVE);
+                        }
                     }
                     return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
                 case 'carrier':
                     numberOfParts = Math.floor((energy - (energy * amountToSave)) / 150);
+                    if (numberOfParts > 0) {
+                        if (numberOfParts > Memory.rooms[room].partsForCarrier) {
+                            numberOfParts = Memory.rooms[room].partsForCarrier;
+                        }
 
-                    if (numberOfParts > Memory.rooms[room].partsForCarrier) {
-                        numberOfParts = Memory.rooms[room].partsForCarrier;
-                    }
-
-                    for (let i = 0; i < numberOfParts; i++) {
-                        body.push(CARRY);
-                        body.push(CARRY);
-                        body.push(MOVE);
+                        for (let i = 0; i < numberOfParts; i++) {
+                            body.push(CARRY);
+                            body.push(CARRY);
+                            body.push(MOVE);
+                        }
                     }
                     return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
                 case 'warrior':
@@ -48,129 +53,147 @@ module.exports = function () {
 
                     if (numberOfRanged <= 3) {
                         numberOfParts = Math.floor((energy - (energy * amountToSave)) / 200);
-                        if (numberOfParts > 5) {
-                            numberOfParts = 5;
-                        }
-                        for (let i = 0; i < numberOfParts; i++) {
-                            body.push(MOVE);
-                            body.push(RANGED_ATTACK);
+                        if (numberOfParts > 0) {
+                            if (numberOfParts > 5) {
+                                numberOfParts = 5;
+                            }
+                            for (let i = 0; i < numberOfParts; i++) {
+                                body.push(MOVE);
+                                body.push(RANGED_ATTACK);
+                            }
                         }
                     }
                     else if (numberOfAttack < 10) {
                         numberOfParts = Math.floor((energy - (energy * amountToSave)) / 210);
-                        if (numberOfParts > 5) {
-                            numberOfParts = 5;
-                        }
-                        for (let i = 0; i < numberOfParts; i++) {
-                            body.push(MOVE);
-                            body.push(ATTACK);
-                            body.push(ATTACK);
+                        if (numberOfParts > 0) {
+                            if (numberOfParts > 5) {
+                                numberOfParts = 5;
+                            }
+                            for (let i = 0; i < numberOfParts; i++) {
+                                body.push(MOVE);
+                                body.push(ATTACK);
+                                body.push(ATTACK);
+                            }
                         }
                     }
                     else {
                         numberOfParts = Math.floor((energy - (energy * amountToSave)) / 300);
-                        for (let i = 0; i < numberOfParts; i++) {
-                            body.push(MOVE);
-                            body.push(HEAL);
+                        if (numberOfParts > 0) {
+                            for (let i = 0; i < numberOfParts; i++) {
+                                body.push(MOVE);
+                                body.push(HEAL);
+                            }
                         }
                     }
                     return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
                 case 'upgrader':
                     numberOfParts = Math.floor(((energy - (energy * amountToSave)) - 100) / 100);
+                    if (numberOfParts > 0) {
+                        if (Memory.rooms[room].energyMode == 'saving' && numberOfParts > 7) {
+                            numberOfParts = 7;
+                        }
+                        if (Memory.rooms[room].energyMode == 'ok' && numberOfParts > 15) {
+                            numberOfParts = 15;
+                        }
 
-                    if (Memory.rooms[room].energyMode == 'saving' && numberOfParts > 7) {
-                        numberOfParts = 7;
+                        body.push(MOVE);
+                        for (let i = 0; i < numberOfParts; i++) {
+                            body.push(WORK);
+                        }
+                        body.push(CARRY);
                     }
-                    if (Memory.rooms[room].energyMode == 'ok' && numberOfParts > 15) {
-                        numberOfParts = 15;
-                    }
-
-                    body.push(MOVE);
-                    for (let i = 0; i < numberOfParts; i++) {
-                        body.push(WORK);
-                    }
-                    body.push(CARRY);
                     return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
                 case 'builder':
                     numberOfParts = Math.floor((energy - (energy * amountToSave)) / 200);
-
-                    for (let i = 0; i < numberOfParts; i++) {
-                        body.push(WORK);
-                        body.push(CARRY);
-                        body.push(MOVE);
+                    if (numberOfParts > 0) {
+                        for (let i = 0; i < numberOfParts; i++) {
+                            body.push(WORK);
+                            body.push(CARRY);
+                            body.push(MOVE);
+                        }
                     }
                     return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
                 case 'repairer':
                     numberOfParts = Math.floor((energy - (energy * amountToSave)) / 200);
-
-                    for (let i = 0; i < numberOfParts; i++) {
-                        body.push(WORK);
-                        body.push(CARRY);
-                        body.push(MOVE);
+                    if (numberOfParts > 0) {
+                        for (let i = 0; i < numberOfParts; i++) {
+                            body.push(WORK);
+                            body.push(CARRY);
+                            body.push(MOVE);
+                        }
                     }
                     return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
                 case 'defenceManager':
                     numberOfParts = Math.floor((energy - (energy * amountToSave)) / 200);
-
-                    for (let i = 0; i < numberOfParts; i++) {
-                        body.push(WORK);
-                        body.push(CARRY);
-                        body.push(MOVE);
+                    if (numberOfParts > 0) {
+                        for (let i = 0; i < numberOfParts; i++) {
+                            body.push(WORK);
+                            body.push(CARRY);
+                            body.push(MOVE);
+                        }
                     }
                     return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
                 case 'landlord':
                     numberOfParts = Math.floor((energy - (energy * amountToSave)) / 650);
-
-                    if (numberOfParts >= 1) {
-                        body.push(CLAIM);
-                        body.push(MOVE);
+                    if (numberOfParts > 0) {
+                        if (numberOfParts >= 1) {
+                            body.push(CLAIM);
+                            body.push(MOVE);
+                        }
                     }
                     return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
                 case 'remoteHarvester':
                     numberOfParts = Math.floor(((energy - (energy * amountToSave)) - 100) / 150);
-                    if (numberOfParts > 7) {
-                        numberOfParts = 7;
-                    }
-                    body.push(CARRY);
-                    body.push(MOVE);
-                    for (let i = 0; i < numberOfParts; i++) {
+                    if (numberOfParts > 0) {
+                        if (numberOfParts > 7) {
+                            numberOfParts = 7;
+                        }
+                        body.push(CARRY);
                         body.push(MOVE);
-                        body.push(WORK);
+                        for (let i = 0; i < numberOfParts; i++) {
+                            body.push(MOVE);
+                            body.push(WORK);
+                        }
                     }
                     return this.createCreep(body, undefined, {role: roleName, room: room.name, goingHome: false});
                 case 'remoteHauler':
                     numberOfParts = Math.floor(((energy - (energy * amountToSave)) - 150) / 150);
-                    body.push(WORK);
-                    body.push(MOVE);
-                    for (let i = 0; i < numberOfParts; i++) {
+                    if (numberOfParts > 0) {
+                        body.push(WORK);
                         body.push(MOVE);
-                        body.push(CARRY);
-                        body.push(CARRY);
+                        for (let i = 0; i < numberOfParts; i++) {
+                            body.push(MOVE);
+                            body.push(CARRY);
+                            body.push(CARRY);
+                        }
                     }
                     return this.createCreep(body, undefined, {role: roleName, room: room.name, goingHome: false});
                 case 'otherRoomCreep':
                     numberOfParts = Math.floor((energy - (energy * amountToSave)) / 200);
-
-                    for (let i = 0; i < numberOfParts; i++) {
-                        body.push(WORK);
-                        body.push(MOVE);
-                        body.push(CARRY);
+                    if (numberOfParts > 0) {
+                        for (let i = 0; i < numberOfParts; i++) {
+                            body.push(WORK);
+                            body.push(MOVE);
+                            body.push(CARRY);
+                        }
                     }
                     return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
                 case 'energyThief':
                     numberOfParts = Math.floor((energy - (energy * amountToSave)) / 100);
-
-                    for (let i = 0; i < numberOfParts; i++) {
-                        body.push(CARRY);
-                        body.push(MOVE);
+                    if (numberOfParts > 0) {
+                        for (let i = 0; i < numberOfParts; i++) {
+                            body.push(CARRY);
+                            body.push(MOVE);
+                        }
                     }
                     return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
                 case 'energyHelper':
                     numberOfParts = Math.floor((energy - (energy * amountToSave)) / 100);
-
-                    for (let i = 0; i < numberOfParts; i++) {
-                        body.push(CARRY);
-                        body.push(MOVE);
+                    if (numberOfParts > 0) {
+                        for (let i = 0; i < numberOfParts; i++) {
+                            body.push(CARRY);
+                            body.push(MOVE);
+                        }
                     }
                     return this.createCreep(body, undefined, {role: roleName, room: room.name, working: false});
                 default:
