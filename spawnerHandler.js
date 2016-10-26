8bb98bd54d72b4bb420a6ca1764f32760a4ffeec
Daniel Reid <danielselareid40@gmail.com>
2016-10-26 18:04:21 +1300

    fixed bug

--- spawnerHandler.js
+++ spawnerHandler.js
@@ -346,16 +346,16 @@ module.exports = {
             var queueUsed = 0; // 0 is normal and 1 is priority 2 is war
 
             if (room.energyCapacityAvailable >= 400) {
-                if (Memory.rooms[room].energyMode == 'saving') {
+                if ((numberOfHarvesters >= minimumNumberOfHarvesters)
+                    && (numberOfDistributors >= minimumNumberOfDistributors)
+                    && (numberOfCarriers >= 2)) {
+                    amountToSave = 0.15;
+                }
+                else if (Memory.rooms[room].energyMode == 'saving') {
                     amountToSave = 0.35;
                 }
                 else if (Memory.rooms[room].energyMode == 'ok') {
                     amountToSave = 0.25;
-                }
-                else if ((numberOfHarvesters >= minimumNumberOfHarvesters)
-                    && (numberOfDistributors >= minimumNumberOfDistributors)
-                    && (numberOfCarriers >= 2)) {
-                    amountToSave = 0.15;
                 }
             }
 
