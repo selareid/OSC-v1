4d3c0d78ee73be10aea8c155e177825f2d49d5ba
Daniel Reid <danielselareid40@gmail.com>
2016-10-26 18:31:00 +1300

    added kraiik to allues list

--- global.js
+++ global.js
@@ -1,4 +1,4 @@
 'use strict';
 
-global.Allies = ['Lur', 'starwar15432', 'Leonyx', 'BuddyNZ', 'Dr_Frizzle', 'TheSirLancelot', 'Sphic'];
+global.Allies = ['Lur', 'starwar15432', 'Leonyx', 'BuddyNZ', 'Dr_Frizzle', 'TheSirLancelot', 'Sphic', 'kraiik'];
 global.CPUUsedByCreepHandler = 0;
