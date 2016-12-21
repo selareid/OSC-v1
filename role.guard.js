e5ac9e88dc480893bfdd3920c48847f8879cabe8
Daniel Reid <danielselareid40@gmail.com>
2016-12-21 19:59:10 +0700

    added that to notes

--- role.guard.js
+++ role.guard.js
@@ -11,6 +11,7 @@ module.exports = {
 
 /*
 *Notes:
+* global[room.name].guardStationFlag for guard station flag
 * global[this.name].creepsNotMine to get hostile creeps in room (includes allies)
 * Memory.rooms[room].isUnderAttack
 */
