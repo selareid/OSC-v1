aa9ee3ba1945fa44c7c4e375e19031df0714ea0e
Daniel Reid <danielselareid40@gmail.com>
2016-12-21 19:37:58 +0700

    added some notes for me while i write this

--- role.guard.js
+++ role.guard.js
@@ -2,8 +2,15 @@ require('global');
 
 module.exports = {
     run: function (room, creep, guardStationFlag) {
-        
+
 
 
     }
 };
+
+
+/*
+*Notes:
+* global[this.name].creepsNotMine to get hostile creeps in room (includes allies)
+* Memory.rooms[room].isUnderAttack
+*/
