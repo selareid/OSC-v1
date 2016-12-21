608bd795c704311afead322ab7fc8670100c9bd2
Daniel Reid <danielselareid40@gmail.com>
2016-12-21 15:23:59 +0700

    updated email notifications for when i’m under attack

--- defenceHandler.js
+++ defenceHandler.js
@@ -4,13 +4,16 @@ const towerHandler = require('towerHandler');
 
 module.exports = {
     run: function (room) {
-        var hostileCreepsInRoom = JSON.stringify(this.getHostileCreeps(room));
+        var hostileCreepsInRoom = this.getHostileCreeps(room);
 
         if (Game.time % 20 == 0) {
             console.log('Enemy creeps spotted in room ' + room);
             console.log("<h1 style=\"color: #ff2f3c\"><strong><i>EMERGENCY MODE </i></strong></h1>\nIn Room: " + room.name + "\nEstimated time of death: " + hostileCreepsInRoom.length * 100 + "ticks" + "\nCreeps remaining: " + _.sum(Game.creeps, (s) => s.room.name === room.name));
 
             Game.notify('Enemy creeps spotted in room ' + room);
+            if (hostileCreepsInRoom[0] && hostileCreepsInRoom[0].owner) {
+                Game.notify('Owner of creep = ' + hostileCreepsInRoom[0].owner.username);
+            }
             Game.notify('Prepare to die future self');
         }
 
