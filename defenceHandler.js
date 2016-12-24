cb8d0ea95f4d01d41633ede0a1ebc55b3cddfdec
Daniel Reid <danielselareid40@gmail.com>
2016-12-24 15:44:57 +0700

    i don’t like email notifications :[

--- defenceHandler.js
+++ defenceHandler.js
@@ -10,7 +10,7 @@ module.exports = {
             console.log('Enemy creeps spotted in room ' + room);
             console.log("<h1 style=\"color: #ff2f3c\"><strong><i>EMERGENCY MODE </i></strong></h1>\nIn Room: " + room.name + "\nEstimated time of death: " + hostileCreepsInRoom.length * 100 + "ticks" + "\nCreeps remaining: " + _.sum(Game.creeps, (s) => s.room.name === room.name));
 
-            if (hostileCreepsInRoom[0] && hostileCreepsInRoom[0].owner) {
+            if (hostileCreepsInRoom[0] && hostileCreepsInRoom[0].owner && hostileCreepsInRoom[0].owner != 'Invader') {
                 Game.notify('Enemy creeps spotted in room ' + room);
                 Game.notify('Owner of creep = ' + hostileCreepsInRoom[0].owner.username);
                 Game.notify('Prepare to die future self');
