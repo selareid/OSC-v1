b25f83cff7e56af9946f42af330fe12b5ca125f7
Daniel Reid <danielselareid40@gmail.com>
2016-12-24 21:39:35 +0700

    trying different things

--- role.signer.js
+++ role.signer.js
@@ -39,12 +39,8 @@ module.exports = {
     },
     
     getRoomToGoTo: function (room, creep) {
-        var exits = Game.map.describeExits(creep.pos.roomName);
-        var exit = _.filter(exits, (e) => !creep.memory.roomsBeenIn.includes(e) && !Game.rooms[e])[0];
-
-        if (Game.time % 20) {
-
-        }
+        var exits = _.filter(Game.map.describeExits(creep.pos.roomName), (e) => !creep.memory.roomsBeenIn.includes(e) && !Game.rooms[e]);
+        var exit = exits[0];
 
         return exit;
     }
