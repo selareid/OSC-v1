9987b5d8401ee302d6f792c6f63b2fe18a533a2c
Daniel Reid <danielselareid40@gmail.com>
2016-12-27 10:39:32 +0700

    new creep structureDestroyer

--- prototype.creepWar.js
+++ prototype.creepWar.js
@@ -5,6 +5,4 @@ module.exports = function () {
                 this.rangedAttack(target);
             }
         };
-
-
 };
---
+++ role.structureDestroyer.js
@@ -0,0 +1,88 @@
+require('prototype.creepSpeech')();
+require('prototype.creepWar')();
+
+module.exports = {
+    run: function (room, creep) {
+        PathFinder.use(true);
+
+        if (creep.memory.team) {
+            var teamGlobal = global['warCache'][creep.memory.team];
+            if (teamGlobal) {
+                var timeToAttack = teamGlobal.timeToAttack;
+                if (timeToAttack != undefined && timeToAttack != null) {
+                    if (Game.time >= timeToAttack) {
+                        var targetFlag = teamGlobal.flag;
+                        var targetRoom = teamGlobal.targetRoom;
+                        if (targetRoom) {
+
+                            if (creep.pos.roomName != targetRoom) {
+                                creep.moveTo(targetFlag)
+                            }
+                            else {
+                                var targetStructure = this.getTargetStructure(creep);
+                                if (targetStructure) {
+                                    var attackStructureResult = creep.attack(targetStructure);
+
+                                    switch (attackStructureResult) {
+                                        case -9: // returns ERR_NOT_IN_RANGE
+                                            creep.moveTo(attackStructureResult, {reusePath: 3, ignoreRoads: true});
+                                            break;
+                                        case 0: // returns OK
+                                            //creep.say something here using prototype.creepSpeech.js
+                                            break;
+                                        default:
+                                            console.log('Error with creep: ' + creep.name + '' + ' Attack Error: ' + attackStructureResult);
+                                    }
+
+                                    creep.basicRangedHandler(targetStructure);
+
+
+                                }
+                                else {
+                                    //maybe add a role changing mechanic here ?
+                                }
+                            }
+
+                        }
+                    }
+                    else {
+                        var timeToRally = Game.time >= teamGlobal.timeToAttack;
+                        if (timeToRally != undefined && timeToRally != null) {
+                            if (Game.time >= timeToRally) {
+                                var rallyFlag = teamGlobal.rallyFlag;
+                                if (rallyFlag) {
+                                    creep.moveTo(rallyFlag, {ignoreRoads: true});
+                                }
+                                else {
+                                    // something here
+                                    creep.moveTo(global[room.name].guardStationFlag);
+                                }
+                            }
+                            else {
+                                creep.moveTo(global[room.name].guardStationFlag);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            console.log('structureDestroyer creep ' + creep.name + ' needs a team');
+            creep.moveTo(global[room.name].guardStationFlag);
+        }
+
+    },
+
+    getTargetStructure: function (creep) {
+        creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS, {filter: (c) => !global.Allies.includes(c.owner.username)});
+    }
+};
+
+
+/*
+*Notes:
+* global['warCache'] war Cache
+* global[room.name].guardStationFlag for guard station flag
+* global[this.name].creepsNotMine to get hostile creeps in room (includes allies)
+* global.Allies
+*/
