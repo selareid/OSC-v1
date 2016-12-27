ac0c4599a921c3528628545a83beb2c84360701c
Daniel Reid <danielselareid40@gmail.com>
2016-12-27 08:27:15 +0700

    maybe fixed maybe broke

--- labHandler.js
+++ labHandler.js
@@ -9,7 +9,8 @@ module.exports = {
         }
 
         var labs = room.find(FIND_MY_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_LAB});
-        global[room.name].resourcesAvailable = [];
+
+        if (Game.time % 2 == 0) Memory.rooms[room].resourcesAvailable = [];
 
         for (let lab of labs) {
             if (!Memory.rooms[room].labs[lab.id]) {
@@ -20,8 +21,8 @@ module.exports = {
             if (!lab.cooldown > 0) {
                 if (Memory.rooms[room].labs[lab.id].type == 1) { // a lab that does the reactions
                     if (lab.mineralAmount < lab.mineralCapacity - 5) {
-                        if (global[room.name].resourcesAvailable.length >= 2) {
-                            var resourcesAvailableGlobal = global[room.name].resourcesAvailable;
+                        if (Memory.rooms[room].resourcesAvailable.length >= 2) {
+                            var resourcesAvailableGlobal = Memory.rooms[room].resourcesAvailable;
                             lab.runReaction(resourcesAvailableGlobal[0], resourcesAvailableGlobal[1]);
                         }
                     }
@@ -29,7 +30,7 @@ module.exports = {
                 else {
                     if (lab.mineralAmount > 5) {
                         if (lab.mineralType) {
-                            global[room.name].resourcesAvailable.push = lab.id;
+                            Memory.rooms[room].resourcesAvailable.push = lab.id;
                         }
                     }
                 }
