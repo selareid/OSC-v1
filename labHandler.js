738496a3b59da06de501a18f6a75bb9966a89632
Daniel Reid <danielselareid40@gmail.com>
2016-12-27 08:48:56 +0700

    rewrote labs sorta

--- labHandler.js
+++ labHandler.js
@@ -10,8 +10,6 @@ module.exports = {
 
         var labs = room.find(FIND_MY_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_LAB});
 
-        if (Game.time % 2 == 0) Memory.rooms[room].resourcesAvailable = [];
-
         for (let lab of labs) {
             if (!Memory.rooms[room].labs[lab.id]) {
                 Memory.rooms[room].labs[lab.id] = {};
@@ -20,17 +18,17 @@ module.exports = {
 
             if (!lab.cooldown > 0) {
                 if (Memory.rooms[room].labs[lab.id].type == 1) { // a lab that does the reactions
-                    if (lab.mineralAmount < lab.mineralCapacity - 5) {
-                        if (Memory.rooms[room].resourcesAvailable.length >= 2) {
-                            var resourcesAvailableGlobal = Memory.rooms[room].resourcesAvailable;
-                            lab.runReaction(resourcesAvailableGlobal[0], resourcesAvailableGlobal[1]);
+                    var labsInRange = lab.findInRange(labs, 2, {filter: (l) => l.id !== lab.id && Memory.rooms[room].labs[l.id].type == 0});
+                    var resourcesAvailable = [];
+                    _.forEach(labsInRange, function (l) {
+                        if (l.mineralAmount > 0) {
+                            resourcesAvailable.push(l);
                         }
-                    }
-                }
-                else {
-                    if (lab.mineralAmount > 5) {
-                        if (lab.mineralType) {
-                            Memory.rooms[room].resourcesAvailable.push = lab.id;
+                    });
+
+                    if (lab.mineralAmount < lab.mineralCapacity - 5) {
+                        if (resourcesAvailable.length >= 2) {
+                            lab.runReaction(resourcesAvailable[0], resourcesAvailable[1]);
                         }
                     }
                 }
