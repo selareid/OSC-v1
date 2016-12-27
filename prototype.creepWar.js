1c21013776a289581e5c22a3eab72ed24933c2d9
Daniel Reid <danielselareid40@gmail.com>
2016-12-27 10:57:47 +0700

    new creep function basic heal

--- prototype.creepWar.js
+++ prototype.creepWar.js
@@ -1,8 +1,19 @@
+require('prototype.creep')();
+
 module.exports = function () {
     Creep.prototype.basicRangedHandler =
         function (target) {
             if (target) {
                 this.rangedAttack(target);
+            }
+        };
+
+        Creep.prototype.basicSelfHeal =
+        function () {
+            if (this.hasActiveBodyparts(HEAL)) {
+                if (this.hits < this.hitsMax) {
+                    this.heal(this);
+                }
             }
         };
 };
